8ef2f61a75378b747c5f295eb55287bd87f4eba7
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-06-04 14:49:06 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,5 @@
 export default async function handler(req, res) {
   res.status(200).json({
-    keyExists: !!process.env.GEMINI_API_KEY,
-    keyStart: process.env.GEMINI_API_KEY?.slice(0,5) || "KOSONG"
+    reply: "TES BERHASIL 🔥"
   });
 }
