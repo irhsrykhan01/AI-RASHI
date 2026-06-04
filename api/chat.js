97444d6545dc51f9ff6fb5b672632282abf31a1e
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-06-04 14:47:28 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,37 +1,6 @@
 export default async function handler(req, res) {
-  try {
-    const { text } = req.body;
-
-    const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          contents: [
-            {
-              parts: [
-                {
-                  text: text
-                }
-              ]
-            }
-          ]
-        })
-      }
-    );
-
-    const data = await response.json();
-
-    res.status(200).json({
-      reply: JSON.stringify(data, null, 2)
-    });
-
-  } catch (error) {
-    res.status(500).json({
-      reply: error.message
-    });
-  }
+  res.status(200).json({
+    keyExists: !!process.env.GEMINI_API_KEY,
+    keyStart: process.env.GEMINI_API_KEY?.slice(0,5) || "KOSONG"
+  });
 }
